19513a48386450fb061ad8d2d16c4d69458f0f0e
donarazhar <donarazhar@gmail.com>
2026-01-20 00:37:01 +0000

    forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        // Force HTTPS untuk semua URL
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
